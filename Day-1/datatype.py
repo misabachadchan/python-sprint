7c27a20dff551078b1ba2c3ce9e77236c6f2df55
misabachadchan <misabachadchan@gmail.com>
2025-09-22 21:57:14 +0530

    day 1

---
+++ Day-1/datatype.py
@@ -0,0 +1,19 @@
+a = 1
+b = "misaba"
+c = True
+d = None
+e = 67.65
+list1 = [8, 2, 3, [4, -5], ["apple", "banana"]]
+# Corrected tuple1: Changed to a tuple of tuples (since it's named 'tuple1' and original syntax was invalid for sets)
+# Also fixed typo "cheery" to "cherry" assuming that's intended
+tuple1 = (("parrot", "sparrow"), ("apple", "cherry"))
+dict1 = {"name": "sakshi", "age": 20, "canvote": True}
+# Types
+print("the type of a is :", type(a))
+print("the type of b is :", type(b))
+print("the type of c is :", type(c))
+print("the type of d is :", type(d))
+print("the type of e is :", type(e))
+print("the type of list1 is :", type(list1))
+print("the type of tuple1 is :", type(tuple1))
+print("the type of dict1 is :", type(dict1))f
---
+++ Day-1/variable.py
@@ -0,0 +1,5 @@
+# valid variables
+a=12
+name="misaba"
+_age=12
+b=12.34
